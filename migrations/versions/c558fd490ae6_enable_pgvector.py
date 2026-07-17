3e7fcc220fcf51cae9d7dd23c693c319cbf1f930
Nicolas Torres <ntorresv.dev@gmail.com>
2026-07-17 02:19:25 -0500

    remove unused import and formatting code

--- migrations/versions/c558fd490ae6_enable_pgvector.py
+++ migrations/versions/c558fd490ae6_enable_pgvector.py
@@ -6,10 +6,7 @@ Create Date: 2026-07-17 01:50:45.159051
 
 """
 from typing import Sequence, Union
-
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'c558fd490ae6'
@@ -17,11 +14,9 @@ down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-
 def upgrade() -> None:
     """Upgrade schema."""
     op.execute("CREATE EXTENSION IF NOT EXISTS vector")
-
 
 def downgrade() -> None:
     """Downgrade schema."""
